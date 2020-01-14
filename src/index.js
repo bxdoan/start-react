244e983cab168e2813ef12f98cc902e8025d6870
doan <bxdoan93@gmail.com>
2020-01-14 14:20:17 +0700

    show 3 cmts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';  // ref. https://github.com/marak/Faker.js/
 
 function getTime() {
     return (new Date()).toLocaleTimeString()
@@ -13,11 +13,11 @@ const App = () => {
         <div className="ui container comments">
             <div className="comment">
                 <a href="/" className="avatar">
-                    <img alt="avatar" />
+                    <img alt="avatar" src={faker.image.avatar()}/>
                 </a>
                 <div className="content">
                     <a href="/" className="author">
-                        Doan
+                        {faker.name.firstName()}
                     </a>
                     <div className="metadata">
                         <span className="date">Today at {getTime()}</span>
@@ -25,6 +25,33 @@ const App = () => {
                     <div className="text"> Nice blog post?</div>
                 </div>
             </div>
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.avatar()}/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        {faker.name.firstName()}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at {getTime()}</span>
+                    </div>
+                    <div className="text"> Nice blog post?</div>
+                </div>
+            </div>            <div className="comment">
+            <a href="/" className="avatar">
+                <img alt="avatar" src={faker.image.avatar()}/>
+            </a>
+            <div className="content">
+                <a href="/" className="author">
+                    {faker.name.firstName()}
+                </a>
+                <div className="metadata">
+                    <span className="date">Today at {getTime()}</span>
+                </div>
+                <div className="text"> Nice blog post?</div>
+            </div>
+        </div>
         </div>
     );
 };
