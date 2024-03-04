546d0c65a681bd68c3c196914ead6dd745ff8625
Doan Bui <bxdoan93@gmail.com>
2024-03-04 15:48:34 +0700

    login

--- src/index.js
+++ src/index.js
@@ -1,29 +1,49 @@
-// Import the React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-// ref. https://github.com/marak/Faker.js/
-import faker from 'faker';
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
+import React, {StrictMode, useState} from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./index.css";
 
-// Create a react component
-const App = () => {
+import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+
+function setToken(userToken) {
+    const token = JSON.stringify(userToken);
+    console.log(`userToken: ${token}`);
+    sessionStorage.setItem('token', token);
+}
+
+function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    console.log(`tokenString: ${tokenString}`);
+    const userToken = JSON.parse(tokenString);
+    console.log(`userToken: ${userToken.token}`);
+    return userToken?.token
+}
+
+function App() {
+    const token = getToken();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
     return (
-        <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:25PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:24PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:23PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:22PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:21PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            <ApprovalCard>
-                <CommentDetail author={faker.name.firstName()} timeAgo="Today at 4:21PM" avatar={faker.image.avatar()} text="Nice blog post?"/>
-            </ApprovalCard>
+        <div className="wrapper">
+            <StrictMode>
+                <h1>Application</h1>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/dashboard" element={<Dashboard />}>
+                        </Route>
+                        <Route path="/login" element={<Login setToken={setToken} />}>
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </StrictMode>
         </div>
     );
-};
+}
 
-// Take the react component and show it on the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+const root = createRoot(document.getElementById("root"));
+root.render(App());
